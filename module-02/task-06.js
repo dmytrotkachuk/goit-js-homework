c787f9093708cf6f74d062e68983292f07fe16cd
dmytrotkachuk <tkachukdimon@gmail.com>
2020-03-09 21:59:35 +0200

    m-2 task-06 final

--- module-02/task-06.js
+++ module-02/task-06.js
@@ -5,32 +5,19 @@ let message;
 
 do {
   message = prompt('Введите число');
-  input = Number(message);
 
-  if (Number.isNaN(Number(message))) {
-    alert('Это не число!');
-  } else {
-    if (message !== null) {
-      numbers.push(input);
+  if (message !== null) {
+    input = Number(message);
+    numbers.push(input);
+    if (Number.isNaN(Number(message))) {
+      alert('Это не число!');
     }
   }
 } while (message !== null);
 
-if (message === null) {
+if (message === null && numbers.length >= 1) {
   for (const number of numbers) {
     total += number;
+    console.log(`Cумма сложения составила " ${total} "`);
   }
 }
-
-if (message === null && numbers < 1) {
-  console.log('Отменено пользователем.');
-} else {
-  console.log(`Cумма сложения составила " ${total} "`);
-}
-
-// Создать сообщение "Введите число"
-// преобразовать введенные данные в число и сохранить в input
-// Ели не число , сказать пользователю
-// Если число, то довавить в массив numbers
-// Если null суммировать всех числа в массиве в переменную total через for...of
-//  Вывести результат
